a178fd2422dcb57aec74ceb9d805fe44551666b3
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-04-09 16:53:45 +0300

    create the game evaluator method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const choices = ['Rock', 'Paper', 'Scissors'];
 
 const computerPlay = () => {
     let randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    return randomNumber;
 }
 
 const clearUserInput = (userInput) => {
@@ -11,4 +11,14 @@ const clearUserInput = (userInput) => {
     return userInput;
 }
 
-console.log(computerPlay());
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection - computerSelection === -1 || playerSelection - computerSelection  === 2) {
+        return `You won!`;
+    } else if (playerSelection - computerSelection === 1 || playerSelection -  computerSelection === -2) {
+        return `You lose!`;
+    } else {
+        return `It's a tie`;
+    }
+}
+
+console.log(playRound(0, computerPlay()));
